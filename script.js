1b2155f623bed16c7e5a2dee04eb9c2b0ab9e156
ShepleySound <shepleysound@gmail.com>
2021-12-01 18:28:14 -0800

    Add user input validation for new grid size

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
+"use strict"
+
 const sketchContainer = document.querySelector(".container")
 
 function drawGrid(size){
     let grid = new Array(size)
-    for (i = 0; i < grid.length; i++){
+    for (let i = 0; i < grid.length; i++){
         grid[i] = new Array(size)
     }
 
     for (let i = 0; i < grid.length; i++){
-        x = i + 1
+        let x = i + 1
         let gridX = grid[i];
-        // gridX = document.createElement("div");
-        // gridX.setAttribute("id", `x${i}`);
-        // gridX.style.display = "flex";
-        // gridX.style.justifyContent = "stretch"
-        // sketchContainer.appendChild(gridX);
         for (let j = 0; j < gridX.length; j++){
-            y = j + 1
+            let y = j + 1
             let gridY = grid[i][j];
             gridY = document.createElement("div");
             gridY.setAttribute("id", `y${y}`);
             gridY.setAttribute("class", `x${x} y${y} grid`);
             gridY.addEventListener("mouseover", () => {randomGridColor(gridY)})
-            // gridY.style.border = gridDisplay(gridSwitch);
             gridY.style.gridRow = `${x}`;
             gridY.style.gridColumn = `${y}`;
             sketchContainer.appendChild(gridY);
         }
     }
-    gridSquares = document.querySelectorAll(".grid");
+    let gridSquares = document.querySelectorAll(".grid");
     return gridSquares;
 }
 
@@ -36,41 +32,43 @@ function randomGridColor(square){
     let randomColor = colors[Math.floor(Math.random() * colors.length)];
     square.style.backgroundColor = randomColor;
 }
-gridSwitch = true;
+let gridSwitch = true;
 drawGrid(16);
 
 
 
 function resetSketch(){
-    let gridSize = prompt("New grid size: ");
-    sketchContainer.innerHTML = ""
-    drawGrid(parseInt(gridSize))
+    let gridSize = prompt("New grid size (1-100):");
 
+    if (gridSize === null){
+        return;
+    }
+    else if (isNaN(gridSize)){
+        alert("Grid size must be a valid number.");
+        resetSketch();
+    }
+    else if (gridSize < 1 || gridSize > 100){
+        alert("Grid size must be between 1 and 100");
+        resetSketch();
+    }
+    else {
+        sketchContainer.innerHTML = ""
+        drawGrid(parseInt(gridSize))
+    }
 }
 
 function gridDisplaySwitch(switchOn){
-    console.log(document.styleSheets[0].rules)
     if (switchOn){
         document.styleSheets[0].deleteRule(0)
         gridButton.textContent = "Grid: OFF"
         return switchOn = false;
     }
     else if (!switchOn){
-        document.styleSheets[0].insertRule('.grid {border: 1px solid grey}', 0)
+        document.styleSheets[0].insertRule('.grid {border: 0px solid grey}', 0)
         gridButton.textContent = "Grid: ON"
         return switchOn = true;
     }
 }
-
-// function gridDisplay(switchOn){
-//     if (switchOn){
-//         border = `1px solid grey`
-//     }
-//     else{
-//         border = "none"
-//     }
-//     return border;
-// }
 
 const clearButton = document.querySelector("#clear-button")
 clearButton.addEventListener("click", resetSketch)
